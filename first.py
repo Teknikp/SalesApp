3df8a35efa75a8062bb22fe6cbd6d163bd458ebc
Bonnaire Fils Emmanuel <42214912+Teknikp@users.noreply.github.com>
2022-11-10 16:14:28 -0400

    Update first.py

--- first.py
+++ first.py
@@ -5,10 +5,14 @@ import pandas as pd
 sl.title('My Finance App')
 df = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
 tickers = df.Symbol.to_list()
+comps = ['Price Comparison', 'Performance Comparison']
 dropdown = sl.multiselect('Choose your  ticker', tickers)
-
+dropdown2 = sl.multiselect()
 start = sl.date_input('Start date', pd.to_datetime('2019-01-01'))
 end = sl.date_input('End date', pd.to_datetime('today'))
+
+
+
 
 def relret(df):
     rel = df.pct_change()
@@ -16,6 +20,20 @@ def relret(df):
     cumret = cumret.fillna(0)
     return cumret
 
+
+
 if len(dropdown) > 0:
     df = relret(yf.download(dropdown, start, end)['Adj Close'])
     sl.line_chart(df)
+    y = dropdown.info
+    mc = dropdown.info['marketCap']
+    fth = dropdown.info['fiftyTwoWeekHigh']
+    ftl = dropdown.info['fiftyTwoWeekLow']
+    pr = dropdown.info['payoutRatio']
+    yi = dropdown.info['yield']
+    pcl = dropdown.info['previousClose']
+    ptb =dropdown.info['priceToBook']
+    d = {'Market Cap': [mc], 'Fifty Two Week High': [fth], 'Fifty Two Week Low': [ftl], 'Price to Book': [ptb], 'Previous Close': [pcl], 'Payout Ratio': [pr], 'Yield': [yi]}
+    df = pd.DataFrame(data=d)
+
+    sl.table(df)
