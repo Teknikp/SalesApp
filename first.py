369eee14357bd1ac33436a54228dc9c36ab2a18b
Bonnaire Fils Emmanuel <42214912+Teknikp@users.noreply.github.com>
2022-11-11 11:36:39 -0400

    Update first.py

--- first.py
+++ first.py
@@ -6,7 +6,7 @@ sl.title('My Finance App')
 df = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
 tickers = df.Symbol.to_list()
 comps = ['Price Comparison', 'Performance Comparison']
-dropdown = sl.multiselect('Choose your  ticker', tickers, max_selections=5)
+dropdown = sl.multiselect('Choose your  ticker', tickers)
 start = sl.date_input('Start date', pd.to_datetime('2019-01-01'))
 end = sl.date_input('End date', pd.to_datetime('today'))
 
